8f85c5bccf780ec8785a7975adebf6ee025d8d15
Greg Federico <gfederico7153@gmail.com>
2019-10-31 15:02:43 -0400

    dotted-underline: pass fill color as property

--- js/dotted-underline.js
+++ js/dotted-underline.js
@@ -1,18 +1,18 @@
 class DottedUnderline {
 
   static get inputProperties() {
-    return ['--number-of-circles'];
+    return ['--number-of-circles', '--fill-color'];
   }
 
   paint(ctx, size, props) {
-    // console.log('ctx', ctx);
+    console.log('ctx', ctx);
     // console.log('size', size);
     // console.log('props', props);
 
     for (let i = 0; i < props.get('--number-of-circles'); i++) {
       let fraction = i * 2;
       ctx.beginPath();
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = props.get('--fill-color');
       ctx.arc(20 + (fraction * 10), size.height - 10, 8, 0, 2 * Math.PI);
       ctx.fill();
     }
